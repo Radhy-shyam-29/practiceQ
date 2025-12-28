58251bfd6d90c7d3b358c5914e781c255cd8cf25
Radhy Shyam <radhy2929@gmail.com>
2025-12-28 16:04:26 +0530

    new day

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 
-let obj = {
-    '3' :'a',
-    '7' : 'rr',
-    'ac' : 'vd',
-    '0' : 'dsvd',
-    '1' : 'devd',
+// let obj = {
+//     3 :'a',
+//     '7' : 'rr',
+//     ac : 'vd',
+//     '0' : 'dsvd',
+//     '1' : 'devd',
 
-}
-console.log(Object.entries(obj));
+
+// }
+// console.log(obj[3]);
+
+// // console.log(Object.assign({aa:3}, {a:2}, {b:1}))
+
+
+// let a = { name: "Radhey", marks: { math: 90 } };
+
+// let b = Object.assign({}, a);
+
+// b.name = "Rahul";
+// b.marks.math = 100;
+
+// console.log(a.name);        // "Radhey"
+// console.log(a.marks.math); // 100 
+
+
+let obj = {
+  data: { score: 10, marks  :  35}
+};
+
+Object.freeze(obj);
+obj.data.score = 99;
+
+console.log(obj);
