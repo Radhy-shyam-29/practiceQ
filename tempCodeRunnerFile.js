98e78c9590531293f2239e5fd1c152eadffdc434
radhy shyam <radhy2929@gmail.com>
2026-01-15 03:10:49 +0530

    goodnight

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,29 @@
 
-// console.log("Hello, World!");
+let arr = [10,20,30,40,50,60,70,80,90,100,110];
+let find = 70;
 
-// aman kumar 1 
-// satender 0
-// pankaj 
+let start = 0;
+let end = arr.length - 1;
+let flag = false;
 
-// // what is terner
+while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] === find) {
+        console.log('index is found at', mid);
+        flag = true;
+        break;
+    } 
+    else if (arr[mid] < find) {
+        start = mid + 1;
+    } 
+    else {
+        end = mid - 1;
+    }
+
+    console.log(arr.slice(start, end + 1));
+}
+
+if (flag === false) {
+    console.log('find value is not in array');
+}
