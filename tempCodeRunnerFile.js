d35cfc22ce3f685a76fe2e14a397dda142604ce3
radhy shyam <radhy2929@gmail.com>
2026-01-31 23:07:46 +0530

    code push

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,29 +1,5 @@
 
-let arr = [10,20,30,40,50,60,70,80,90,100,110];
-let find = 70;
-
-let start = 0;
-let end = arr.length - 1;
-let flag = false;
-
-while (start <= end) {
-    let mid = Math.floor((start + end) / 2);
-
-    if (arr[mid] === find) {
-        console.log('index is found at', mid);
-        flag = true;
-        break;
-    } 
-    else if (arr[mid] < find) {
-        start = mid + 1;
-    } 
-    else {
-        end = mid - 1;
-    }
-
-    console.log(arr.slice(start, end + 1));
-}
-
-if (flag === false) {
-    console.log('find value is not in array');
+// optional: agar sorted chahiye
+for (let i = 0; i < 3; i++) {
+  buckets[i].sort((a, b) => a - b)
 }
