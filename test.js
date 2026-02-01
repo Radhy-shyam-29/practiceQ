4f29d97bd43d49f6b3725c377ff9a41f134dce82
radhy shyam <radhy2929@gmail.com>
2026-02-02 03:43:25 +0530

    pushing now

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
-
 // // whart is javascript ? and what is the use of javascript ?
 // // JavaScript is a versatile programming language primarily used for creating interactive and dynamic content on websites. It allows developers to implement complex features such as animations, form validations, and real-time updates, enhancing user experience. Additionally, JavaScript can be used for server-side development through environments like Node.js, making it a full-stack development language. Its widespread use and extensive libraries make it a fundamental tool in modern web development.
 
 // // console.log("Hello, World!");
 
-// // aman kumar 1 
+// // aman kumar 1
 // // satender 0
-// // pankaj 
+// // pankaj
 
 // // // what is ternery operator ?
 // // The ternary operator is a concise way to perform conditional expressions in programming languages like JavaScript. It takes three operands: a condition, a value if the condition is true, and a value if the condition is false. The syntax is: condition ? valueIfTrue : valueIfFalse. It allows for shorter and more readable code compared to traditional if-else statements when assigning values based on conditions.
@@ -16,14 +15,12 @@
 // // let canVote = (age >= 18) ? "Yes, you can vote." : "No, you cannot vote.";
 // // console.log(canVote); // Output: Yes, you can vote.
 
-
 // // let a = 5
 // // console.log(a++) // Output: 5
 // // console.log(a++) // Output: 6
 // // console.log(a--) // Output: 7
 // // console.log(--a) // Output: 6
 // // console.log(a++) // Output: 7
-
 
 // function isLeapYear(year) {
 //     if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
@@ -37,7 +34,6 @@
 // console.log(isLeapYear(1900))
 // console.log(isLeapYear(2021))
 
-
 // // diff between deep copy and shallow copy ?
 // // A shallow copy creates a new object that references the same memory locations as the original object for nested objects, meaning changes to nested objects in the copy will affect the original. A deep copy, on the other hand, creates a completely independent copy of the original object and all of its nested objects, so changes to the copy do not affect the original.
 
@@ -45,7 +41,7 @@
 // let original = { a: 1, b: { c: 2 } };
 // let shallowCopy = Object.assign({}, original);
 // shallowCopy.b.c = 3;
-// console.log(original.b.c); 
+// console.log(original.b.c);
 
 // // example of deep copy
 // let deepCopy = JSON.parse(JSON.stringify(original));
@@ -59,7 +55,6 @@
 // let result = arr.flatMap(x => [x, x * 2]);
 // console.log(result); // Output: [1, 2, 2, 4, 3, 6]
 
-
 // function isPrime(num) {
 //     if (num <= 1) return 'not a prime number';
 //     for (let i = 2; i <= Math.sqrt(num); i++) {
@@ -68,9 +63,6 @@
 //     return 'prime number';
 // }
 // console.log(isPrime(11))
-
-
-
 
 // function secondLargest(arr) {
 //     let first = -Infinity;
@@ -86,7 +78,6 @@
 
 //     return second === -Infinity ? 'No second largest number' : second;
 // }
-
 
 // let str = 'ramesh'
 // let resulta = ''
@@ -116,15 +107,15 @@
 //     return value
 //   }
 //   let deepest = Array.isArray(value) ? [] : {}
-  
+
 //   for(let key in value){
 //     deepest[key]  = checking(value[key]);
 //   }
 //   return deepest
 //   }
-  
+
 //   let deepcopy = checking(data);
-  
+
 // function sum(...a){
 //     let total = 0
 //     for(let num of a){
@@ -140,7 +131,6 @@
 
 // console.log(sum())
 
-
 // function sum(...a) {
 //   let total = 0;
 
@@ -154,8 +144,7 @@
 //   return total;
 // }
 
-// console.log(sum(1, 2, 3, 4)); 
-
+// console.log(sum(1, 2, 3, 4));
 
 // const defaults = { theme: "dark", lang: "en", notifications: true };
 // const userPrefs = { theme: "light", fontSize: 14 };
@@ -172,8 +161,6 @@
 
 // saveSettings(defaults, userPrefs);
 
-
-
 // let n = 5
 
 // for(let i=0; i<=n; i++){
@@ -183,7 +170,6 @@
 // }
 // console.log(result)
 // }
-
 
 //     A
 //    B C B
@@ -208,4 +194,191 @@
 //     console.log(result)
 // }
 
+// console.log(String.fromCharCode(72), String.fromCharCode(69), String.fromCharCode(76), String.fromCharCode(76), String.fromCharCode(79), " ", String.fromCharCode(87), String.fromCharCode(79), String.fromCharCode(82), String.fromCharCode(76), String.fromCharCode(68))
 
+// let arr = [
+//   (String.fromCharCode(65 + 7)),
+//   (String.fromCharCode(65 + 4)),
+//   (String.fromCharCode(65 + 11)),
+//   (String.fromCharCode(65 + 11)),
+//   (String.fromCharCode(65 + 14)),
+//   (String.fromCharCode(32)),
+//   (String.fromCharCode(65 + 22)),
+//   (String.fromCharCode(65 + 14)),
+//   (String.fromCharCode(65 + 17)),
+//   (String.fromCharCode(65 + 11)),
+//   (String.fromCharCode(65 + 3)),
+// ];
+
+// console.log(arr.join(''))
+
+// let codes = [7,4,11,11,14,22,14,17,11,3]
+// let result = ''
+// for(let i=0; i<codes.length; i++){
+// result += String.fromCharCode(65+codes[i])
+// }
+// console.log(result)
+
+// let s = new Set()
+
+// s.add(100)
+// s.add(20)
+// s.add(30)
+
+// console.log(s)
+
+// let arr = [10,20,30,20,100,20,30,40,50,10,20]
+
+// let s = new Set()
+
+// for(let t of arr){
+//     s.add(t)
+// }
+// console.log(s)
+
+// let str = "fjshklad";
+// let s = new Set(str);
+// if (str.length == s.size) {
+//   console.log("no repeating");
+// } else {
+//   console.log("repeating");
+// }
+
+// let alphabets = 'qwertyuiopasdfghjklzxcvbnm'
+
+// if(alphabets >= 97 && alphabets <= 122){
+//     console.log('only alphabets')
+// } else{
+//     console.log('something else')
+// // }
+
+// let arr = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// for (let i = 0; i < arr.length; i++) {
+//   let result = 0;
+//   for (let j = 0; j < arr[i].length; j++) {
+//     result +=arr[i][j];
+    
+// }
+// console.log(result);
+// }
+
+// let arr = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
+// for (let i = 0; i < arr[0].length; i++) {
+//   let result = 0;
+
+//   for (let j = 0; j < arr.length; j++) {
+//     result += arr[j][i];
+//   }
+
+//   console.log(result);
+// }
+
+
+// let rows = arr.length
+// let col = arr[0].length
+
+// for(let i=0; i<rows; i++){
+//     let result = 0
+//     for(let j=0; j<col; j++){
+//         result += arr[j][i]
+//     }
+//     console.log(result)
+// }
+
+// let arr = [1,2,4,23,3,4,5,6,45,4,3,2,21,3,4,5,56,6]
+// let freq = {}
+
+// for(let i=0; i<arr.length; i++){
+//     if(freq[arr[i]]){
+//         freq[arr[i]]++
+//     }
+//     else{
+//         freq[arr[i]] = 1
+//     }
+// }
+// console.log(freq)
+
+// let arr = [1,2,4,23,3,4,5,6,45,4,3,2,21,3,4,5,56,6]
+
+// let freq = new Map()
+
+// for(let i=0; i<arr.length; i++){
+//     if(freq.has(arr[i])){
+//         freq.set(arr[i], freq.get(arr[i]) + 1)
+//     }
+//     else{
+//         freq.set(arr[i] , 1)
+//     }
+// }
+// console.log(freq)
+
+// let arr = [10,21,30,43,50]
+
+// let mapwala = arr.filter((value) => {
+//     if(value%2==0){
+//         return value
+//     }
+// })
+
+// console.log(mapwala)
+
+// let mapa = []
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i] % 2 == 0){
+//         mapa.push(arr[i])
+//     }
+// }
+// console.log(mapa)
+
+
+// let arr = [
+//   { name: "Ramesh", age: 18 },
+//   { name: "Mohan",  age: 16 },
+//   { name: "Sohan",  age: 46 },
+//   { name: "Vinod",  age: 20 }
+// ];
+
+// let x = arr.filter((value) => {
+//     if(value.age > 18){
+//         return value.age
+//     }
+// })
+// console.log(x)
+
+
+let countries = [
+  "India",
+  "United States",
+  "Canada",
+  "United Kingdom",
+  "Australia",
+  "Germany",
+  "France",
+  "Japan",
+  "China",
+  "Brazil",
+  "South Africa",
+  "Italy"
+];
+
+// for(let i=0; i<countries.length; i++){
+//     if(countries[i].includes('a') && countries[i].includes('i')){
+//        console.log(countries[i])
+//     }
+// }
+
+let x = countries.filter((value) => {
+     if(value.includes('a') && value.includes('i')){
+     return value
+ }
+})
+console.log(x)
